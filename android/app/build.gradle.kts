33369d83e83e5f517162f73a28cce4e46f42961a
Rajaatawe <rajaataw0@gmail.com>
2025-12-10 11:42:09 +0300

    make notification with firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    kotlin("android")   // ← الصحيح
+    id("com.google.gms.google-services")  // صحيح في Kotlin DSL
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -10,33 +10,44 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.internet_application_project"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            // ضع signing الحقيقي لاحقًا
         }
     }
+}
+
+dependencies {
+    // Firebase BOM
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+
+    // Firebase products
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-messaging")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-installations")   // مهم
+
+    // Kotlin desugaring
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
 
 flutter {
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:internet_application_project/features/Form/presentation/preview.dart';
-import 'package:internet_application_project/features/auth/presentation/login_page.dart';
-import 'package:internet_application_project/features/home_page/presentation/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:internet_application_project/features/notification_page/NotificationService.dart';
+import 'features/auth/presentation/register.dart';
 
-import 'features/auth/presentation/login_page.dart';
 
-void main() {
+Future<void> safeFirebaseInit() async {
+  if (Firebase.apps.isEmpty) {
+    await Firebase.initializeApp(
+      // options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+}
+
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await safeFirebaseInit();
+
+  print("📩 Background Notification:");
+  print("Title: ${message.notification?.title}");
+  print("Body: ${message.notification?.body}");
+}
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await safeFirebaseInit();
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.microtask(() => NotificationService().init());
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       theme: ThemeData(
-        fontFamily: 'Outfit',
-
-      ),
+      theme: ThemeData(fontFamily: 'Outfit'),
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: const RegisterPage(),
     );
   }
 }
